abee8f051d6333f195338a38bd47d57252c1fabf
Sandesh Prabhu <sandeshprabhu.n@gmail.com>
2018-02-19 02:38:45 +0530

    Created a new PWA for Weather Data

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+/*Service worker*/
+
+var cacheName = 'weatherPWA-v6';
+var dataCachename = 'weatherData-v6';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/localforage.min.js',
+  '/styles/ud811.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
+
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(key !== cacheName) {
+          console.log('[ServiceWorker] removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] fetch', e.request.url);
+  if (e.request.url.startsWith(weatherAPIUrlBase)) {
+    e.respondWith(
+      fetch(e.request).then(function(response) {
+        return caches.open(dataCachename).then(function(cache) {
+          cache.put(e.request.url, response.clone());
+          console.log('[ServiceWorker] fetched and cached', e.request.url);
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
